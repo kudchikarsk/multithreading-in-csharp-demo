9fd2de9642af03bf231ee08c897af5f533188764
Shadman Kudchikar <kudchikarsk@gmail.com>
2018-03-02 16:39:43 +0530

    Add project files.

---
+++ NonResponsiveAppDemo/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReponsiveAppDemo
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread.Sleep(5000);
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text += "You Clicked Me!\n";
+        }
+    }
+}
